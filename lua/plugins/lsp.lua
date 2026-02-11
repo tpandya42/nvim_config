34f02d08b3f94f17ccbaa5210f1836a4545e2fa9
Tanmay <tanmaypandya159@gmail.com>
2026-02-11 13:03:22 +0100

    Adding Mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,41 +1,62 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
         config = function()
             local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            -- 1. LSP Servers configuration
+            -- Diagnostics UI
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+            })
+
+            -- On attach keymaps
+            local function on_attach(_, bufnr)
+                local opts = { buffer = bufnr }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>f", function()
+                    vim.lsp.buf.format({ async = true })
+                end, opts)
+            end
+
+            -- Server definitions
             local servers = {
-                html = {},
-                cssls = {},
-                tailwindcss = {},
-                emmet_language_server = {},
-                eslint = {},
                 clangd = {},
-                bashls = {},
-                tsserver = {
-                    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
-                    settings = { javascript = { implicitProjectConfiguration = { checkJs = true } } }
-                },
-                pyright = { filetypes = { "python" }, settings = { python = { analysis = { typeCheckingMode = "basic" } } } },
-                gopls = {
-                    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                ts_ls = {},
+                pyright = {
                     settings = {
-                        gopls = {
-                            analyses = { unusedparams = true, shadow = true, unusedwrite = true, nilness = true },
-                            staticcheck = true,
-                            gofumpt = true,
+                        python = {
+                            analysis = { typeCheckingMode = "basic" },
                         },
                     },
                 },
+                bashls = {},
+                jdtls = {},
                 lua_ls = {
-                    filetypes = { "lua" },
-                    settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+                    settings = {
+                        Lua = {
+                            diagnostics = { globals = { "vim" } },
+                        },
+                    },
                 },
             }
 
             for name, config in pairs(servers) do
                 config.capabilities = capabilities
+                config.on_attach = on_attach
+
                 vim.lsp.config(name, config)
                 vim.lsp.enable(name)
             end
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup({
+                ui = { border = "rounded" },
+            })
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "clangd",
+                    "ts_ls",
+                    "pyright",
+                    "bashls",
+                    "jdtls",
+                    "lua_ls",
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+}
